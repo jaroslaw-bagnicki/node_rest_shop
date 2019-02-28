7f7900fba4ed0c13dbf81f37a9d514b19beffc0f
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-28 14:12:28 +0100

    Add hanling not found resource and error handling

--- app.js
+++ app.js
@@ -13,4 +13,19 @@ app.use(morgan('dev'));
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
 
+// Handling resource not found
+app.use((req, res, next) => {
+  const error = new Error('Not found.');
+  error.status = 404;
+  next(error);
+});
+
+// Handling errors
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({error : {
+    message: error.message
+  }});
+});
+
 module.exports = app;
