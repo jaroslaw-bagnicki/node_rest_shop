378296a0e9c9aa33c9bc044e193445959cbc3806
Jarosław Bagnicki <jarek@bagnicki.net>
2019-03-01 10:54:53 +0100

    Setup db connection

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const setupHeaders = require('./api/middlewares/setupHeaders');
 const notFoundError = require('./api/middlewares/notFoundError');
@@ -17,6 +18,11 @@ app.use(morgan('dev'));
 // Setup body parsing
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+mongoose.connect(process.env.DB_URL + process.env.DB_NAME, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('connected', () => console.log('Connected to: ' + process.env.DB_URL + process.env.DB_NAME));
+db.on('error', (err) => console.error(err));
 
 // Setup respons headers
 app.use(setupHeaders);
