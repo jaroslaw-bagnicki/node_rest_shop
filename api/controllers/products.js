ef98856ab0794ba053b3203a7f5f3b599250c5e5
Jarosław Bagnicki <jarek@bagnicki.net>
2019-03-01 12:46:51 +0100

    Update addProduct method in controller. Handle db saving to db

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -1,11 +1,23 @@
+const mongoose = require('mongoose');
+const Product = require('../models/product');
+
 exports.addProduct = (req, res, next) => {
-  const product = {
+  const product = new Product({
+    _id: mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price,
     quantity: req.body.quantity || 0
-  };
-  return res.status(201).json({
-    message: 'Product created.',
-    product
   });
+  product.save()
+    .then(result => {
+      console.log('Product saved. Id: ', result._id);
+      return res.status(201).json({
+        message: 'Product created.',
+        product
+      });
+    })
+    .catch(error => {
+      console.error('Error: ', error.errmsg);
+      next(error);
+    });
 };
