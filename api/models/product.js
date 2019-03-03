498795e0e8729727b1e222d7f0b72e2fe4edbdb9
Jarosław Bagnicki <jarek@bagnicki.net>
2019-03-03 15:32:11 +0100

    Disable versioning for Product model

--- api/models/product.js
+++ api/models/product.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
-const productSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  name: String,
-  price: Number,
-  quantity: Number
-});
+const productSchema = mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    name: String,
+    price: Number,
+    quantity: Number
+  },
+  {
+    versionKey: false
+  }
+);
 
 module.exports = mongoose.model('Product', productSchema, 'products');
