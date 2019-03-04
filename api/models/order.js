b2edd0c67dc3d4efa795e3ec08d7ba6d43b17490
Jarosław Bagnicki <jarek@bagnicki.net>
2019-03-04 13:15:57 +0100

    Refactor order model

--- api/models/order.js
+++ api/models/order.js
@@ -2,17 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const orderItem = new Schema({
-  _id: { type: ObjectId, select: false },
-  product: { type: ObjectId,  ref: 'Product' },
-  quantity: { type: Number, default: 0 }
-},
-{
-  versionKey: false
-});
+const orderItemSchema = new Schema(
+  {
+    _id: { type: ObjectId, select: false },
+    product: { type: ObjectId,  ref: 'Product' },
+    quantity: { type: Number, default: 0 }
+  },
+  { versionKey: false }
+);
 
 const orderSchema = new Schema(
-  { items: [orderItem] },
+  { 
+    createAt: { type: Date, default: Date.now },
+    items: [orderItemSchema] 
+  },
   { versionKey: false }
 );
 
